a37a9b31d0bf5ce416e6561ab4acae06ef8b2fe3
pabloOcanto <pomalianni@gmail.com>
2021-11-17 15:08:52 -0300

    agregado anotation order

--- src/main/java/com/transtour/backend/zulproxy/config/WebSecurityConfig.java
+++ src/main/java/com/transtour/backend/zulproxy/config/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package com.transtour.backend.zulproxy.config;
 
 import com.transtour.backend.zulproxy.filter.JWTAuthorizationFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
+@Order(1)
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
