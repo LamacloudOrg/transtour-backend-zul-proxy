b3f35afecce09247486950c7dd3fb68c8ad72c71
pabloOcanto <pomalianni@gmail.com>
2021-11-17 15:36:27 -0300

    add header authorization

--- src/main/java/com/transtour/backend/zulproxy/config/SimpleCORSFilter.java
+++ src/main/java/com/transtour/backend/zulproxy/config/SimpleCORSFilter.java
@@ -30,9 +30,9 @@ public class SimpleCORSFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD, PATCH, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
 
         chain.doFilter(req, res);
     }
