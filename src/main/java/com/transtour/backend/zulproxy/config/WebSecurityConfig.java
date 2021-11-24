b47fdce9e86b4f6dc8ebddc2dd47fe05d9b5372b
pabloOcanto <pomalianni@gmail.com>
2021-11-24 14:13:43 -0300

    allow specific origins

--- src/main/java/com/transtour/backend/zulproxy/config/WebSecurityConfig.java
+++ src/main/java/com/transtour/backend/zulproxy/config/WebSecurityConfig.java
@@ -13,7 +13,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +41,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://transtour.com.ar"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("HEAD");
