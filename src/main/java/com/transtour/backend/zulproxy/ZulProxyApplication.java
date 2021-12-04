3ce797da588b0708eb9aae970e0f108101ab080b
charlyla <cnlaffitte@gmail.com>
2021-12-04 18:14:25 -0300

    add cors configuration

--- src/main/java/com/transtour/backend/zulproxy/ZulProxyApplication.java
+++ src/main/java/com/transtour/backend/zulproxy/ZulProxyApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,4 +17,13 @@ public class ZulProxyApplication {
 		SpringApplication.run(ZulProxyApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://209.126.85.7:8080").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
